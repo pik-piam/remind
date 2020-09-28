7daa273bbec90578f99719068c33365a3a3b10e0
Alois Dirnaichner <alodi@directbox.com>
2020-09-28 16:41:24 +0200

    Add also the correct *complex* LDV biomass variable.

--- R/reportCrossVariables.R
+++ R/reportCrossVariables.R
@@ -143,8 +143,8 @@ reportCrossVariables <- function(gdx,output=NULL,regionSubsetList=NULL){
   if(tran_mod == "complex"){
       tmp <- mbind(tmp,setNames(
                            output[r,,"FE|Transport|Pass|Road|LDV|Liquids (EJ/yr)"] 
-                           * output[r,,"SE|Liquids|Biomass (EJ/yr)"]
-                           / output[r,,"SE|Liquids (EJ/yr)"],                     "FE|Transport|Pass|Road|LDV|Liquids|Biomass (EJ/yr)"))
+                           * output[r,,"FE|Transport|Pass|Liquids|Biomass (EJ/yr)"]
+                           / output[r,,"FE|Transport|Pass|Liquids (EJ/yr)"],                     "FE|Transport|Pass|Road|LDV|Liquids|Biomass (EJ/yr)"))
       tmp <- mbind(tmp,setNames(
                            output[r,,"FE|Transport|Pass|Road|LDV|Liquids (EJ/yr)"] 
                            * output[r,,"SE|Liquids|Coal (EJ/yr)"]
